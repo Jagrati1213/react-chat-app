ce141f2ee5799dcb0eaf124946215d1141121234
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-02 19:23:02 +0530

    jg: add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import { Signin } from './components/auth/Signin';
+import { Login } from './components/auth/Login';
+import Home from './components/chatBox/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Signin />
-    </div>
+
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
