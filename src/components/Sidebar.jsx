8f05ecdc56abeaf679dd985bae3f6816cd4ae9ab
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-30 13:36:41 +0530

    jg: remove space

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,24 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import UserHeader from './UserHeader'
-import SearchUser from './SearchUser'
-import UserCard from './UsersCard'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import UserHeader from "./UserHeader";
+import SearchUser from "./SearchUser";
+import UserCard from "./UsersCard";
 
 function Sidebar() {
   return (
-    <Box flex='1' bg='white' height='-webkit-fill-available' borderRadius='5px' borderRight={'1px solid lightgray'}>
-         <UserHeader/>
-         <SearchUser/>
-         <UserCard/>
+    <Box
+      flex="1"
+      bg="white"
+      height="-webkit-fill-available"
+      overflow={"hidden"}
+      borderRadius="5px"
+      borderRight={"1px solid lightgray"}
+    >
+      <UserHeader />
+      <SearchUser />
+      <UserCard />
     </Box>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
