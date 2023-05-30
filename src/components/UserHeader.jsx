f383f221e59b89b841d5a6653a8c6b7c073fad8d
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-30 14:42:37 +0530

    jg: remove space

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,41 +1,49 @@
-import React from 'react'
-import { Flex, Text,Avatar, Box, Stack } from '@chakra-ui/react'
+import React from "react";
+import { Flex, Text, Avatar, Box, Stack, Switch } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa";
 // import { signOut } from 'firebase/auth'
 // import { auth } from '../Firebase'
-import { IconContext } from 'react-icons';
+import { IconContext } from "react-icons";
 
 function UserHeader() {
-    const logoutBtn={
-        color:'#54656f',
-        cursor:'pointer',
-        ':hover':{
-            bg:'#f0f2f5',
-        }
-    }
-    
+  const logoutBtn = {
+    color: "#54656f",
+    cursor: "pointer",
+    ":hover": {
+      bg: "#f0f2f5",
+    },
+  };
 
   return (
-    <Flex  
-     alignItems='center' 
-     justifyContent='space-between' 
-     bg='#f0f2f5' 
-     padding='10px' 
-     color='white'  
-     borderRadius='10px 0 0 0'>
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      bg="#f0f2f5"
+      padding="10px"
+      color="white"
+      borderRadius="10px 0 0 0"
+    >
+      <Stack alignItems="center" flexDirection="row" wrap="wrap">
+        <Avatar src="" />
+        <Text paddingInlineStart="12px" color={"#54656f"} fontWeight="500">
+          Jagrati Gupta
+        </Text>
+      </Stack>
 
-       <Stack alignItems='center' flexDirection='row' wrap='wrap' >
-           <Avatar src=''/>
-           <Text paddingInlineStart='12px' color={'#54656f'} fontWeight='500'>Jagrati Gupta</Text>
-       </Stack>
+      <Stack
+        alignItems="flex-end"
+        flexDirection="row"
+        wrap="wrap"
+        justifyContent={"center"}
+      >
+        <Switch size="sm" padding={"0 12px"} />
 
-       <Stack alignItems='center' flexDirection='row' wrap='wrap' >
-          <IconContext.Provider value={{size:'20px'}}><FaPlus color='#54656f'/></IconContext.Provider>       
-       </Stack>
-   
-         
+        <IconContext.Provider value={{ size: "20px" }}>
+          <FaPlus color="#54656f" />
+        </IconContext.Provider>
+      </Stack>
     </Flex>
-  )
+  );
 }
 
 export default UserHeader;
