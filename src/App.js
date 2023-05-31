7048f5af7a014a6c64c9d6a867debc58754aa18a
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-31 14:03:03 +0530

    jg: add signin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from './Page/Login';
 import Home from './Page/MainPage';
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Sign from './Page/Sign';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/sigin' element={<Sign />} />
         </Routes>
         <Toaster />
       </div>
