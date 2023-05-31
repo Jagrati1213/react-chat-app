60467bfbb7d8c342999f1cde5760b0275f80166c
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-31 14:02:49 +0530

    jg: add user reducer

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import DarkModeSlice from './slice/DarkModeSlice';
+import AddUserSlice from './slice/AddUserSlice';
 
 const store = configureStore({
     reducer: {
         darkMode: DarkModeSlice,
+        users: AddUserSlice,
     }
 });
 
