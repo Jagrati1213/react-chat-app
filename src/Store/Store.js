d3296a9c9c93808b4e31162c6a38dbdefa5d4903
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-01 12:48:20 +0530

    jg: add middleware

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,12 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit'
 import DarkModeSlice from './slice/DarkModeSlice';
 import AddUserSlice from './slice/AddUserSlice';
+import thunk from 'redux-thunk';
 
 const store = configureStore({
     reducer: {
         darkMode: DarkModeSlice,
         users: AddUserSlice,
-    }
-});
+    },
+    // for srializable error
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+}
+);
 
 export default store;
