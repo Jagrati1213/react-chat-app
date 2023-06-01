4892021796cfbf82a02e1039c7409fc1db6ed248
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-01 12:49:33 +0530

    jg: add logout button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import React from "react";
 import UserHeader from "./UserHeader";
 import SearchUser from "./SearchUser";
 import UserCard from "./UsersCard";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../Store/slice/AddUserSlice";
 
 function Sidebar() {
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch(removeUser());
+  };
   return (
     <Box
       flex="1"
       bg="white"
       height="-webkit-fill-available"
-      overflow={"hidden"}
+      overflow-x={"hidden"}
       className="sidebar"
     >
       <UserHeader />
       <SearchUser />
       <UserCard />
+      <Box h={"auto"} w={"100%"}>
+        <Button
+          colorScheme="facebook"
+          className="w-full"
+          borderRadius={"none"}
+          onClick={logout}
+        >
+          Logout
+        </Button>
+      </Box>
     </Box>
   );
 }
