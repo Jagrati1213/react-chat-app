3d927c66c27c6fbfd9da81ff24f7d4e8a8684ffe
Jagrati Gupta <123jagratigupta@gmail.com>
2023-06-01 12:48:52 +0530

    jg: add currentuser & condition

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import Home from './Page/MainPage';
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Sign from './Page/Sign';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentUser } from './Store/slice/AddUserSlice';
+import { auth } from './firebaseStore/Firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
 
 function App() {
 
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.users);
+
+  // Check is user Exit or not 
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      dispatch(setCurrentUser(user));
+    });
+    return () => unsub();
+  }, []);
   return (
 
     <Router>
       <div className="App">
         <Routes>
-          <Route index element={<Home />} />
+          <Route index element={currentUser ? <Home /> : <Login />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/sigin' element={<Sign />} />
+          <Route path='/signin' element={<Sign />} />
         </Routes>
         <Toaster />
       </div>
